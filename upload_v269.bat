481d8c20a32e190fdac186e4bd34f466efa89d8c
Robert McLeod <robbmcleod@gmail.com>
2018-12-20 16:20:50 -0800

    Updates for 2.6.9

--- upload_v269.bat
+++ upload_v269.bat
@@ -1,4 +1,4 @@
-set VERSION=2.6.8
+set VERSION=2.6.9
 set CDN_URL=http://a365fff413fe338398b6-1c8a9b3114517dc5fe17b7c3f8c63a43.r19.cf2.rackcdn.com/
 python wheel-uploader.py -r pypi -u %CDN_URL% -v -w ./wheelhouse -t macosx numexpr %VERSION%
 python wheel-uploader.py -r pypi -u %CDN_URL% -v -w ./wheelhouse -t manylinux1 numexpr %VERSION%
